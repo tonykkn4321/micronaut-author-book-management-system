e8259a5d2f487375c194dea25b444c9632c4085c
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-08 23:01:49 +0800

    Update BookRepository.java

--- src/main/java/my/app/repositories/BookRepository.java
+++ src/main/java/my/app/repositories/BookRepository.java
@@ -1,8 +1,11 @@
 package my.app.repositories;
 
-import my.app.models.Book;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
+import my.app.models.Book;
+import java.util.List;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {}
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByAuthorId(Long authorId); // ← This is the missing method
+}
