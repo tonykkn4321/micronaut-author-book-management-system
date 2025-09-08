beb6268ffd752fec9fe30f8d7aefceb9fb3f437f
Tony Kwong <tonykkn4321@gmail.com>
2025-09-08 14:44:08 +0800

    Downgrade the java version to ver.17 for deploying to Railway

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,34 +13,22 @@ repositories {
 }
 
 dependencies {
-    // Annotation processors
     annotationProcessor("io.micronaut.data:micronaut-data-processor")
     annotationProcessor("io.micronaut:micronaut-http-validation")
     annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
 
-    // Micronaut core
     implementation("io.micronaut:micronaut-runtime")
     implementation("io.micronaut.serde:micronaut-serde-jackson")
     implementation("io.micronaut.sql:micronaut-jdbc-hikari")
     implementation("io.micronaut.data:micronaut-data-jdbc")
-
-    // Jakarta Persistence API
     implementation("jakarta.persistence:jakarta.persistence-api:3.1.0")
-
-    // HTTP client
     implementation("io.micronaut:micronaut-http-client")
 
-    // Logging
     runtimeOnly("ch.qos.logback:logback-classic")
-
-    // Database drivers (choose one or both)
-    runtimeOnly("com.mysql:mysql-connector-j")              
-    runtimeOnly("org.postgresql:postgresql:42.7.1")         
-
-    // YAML support
+    runtimeOnly("com.mysql:mysql-connector-j")
+    runtimeOnly("org.postgresql:postgresql:42.7.1")
     runtimeOnly("org.yaml:snakeyaml:2.2")
 
-    // Testing
     testImplementation("io.micronaut.test:micronaut-test-junit5")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
@@ -51,8 +39,8 @@ application {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 graalvmNative {
@@ -79,5 +67,5 @@ micronaut {
 }
 
 tasks.named<io.micronaut.gradle.docker.NativeImageDockerfile>("dockerfileNative") {
-    jdkVersion.set("21")
+    jdkVersion.set("17")
 }
