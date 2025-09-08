9052b57d4e06a918de3b79ecb50d15ab7ad2451a
tonykkn4321 <tonykkn4321@gmail.com>
2025-09-08 22:52:16 +0800

    Update Author.java

--- src/main/java/my/app/models/Author.java
+++ src/main/java/my/app/models/Author.java
@@ -1,8 +1,6 @@
 package my.app.models;
 
-import io.micronaut.data.annotation.GeneratedValue;
-import io.micronaut.data.annotation.Id;
-import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.*;
 
 @MappedEntity
 public class Author {
@@ -14,31 +12,13 @@ public class Author {
     private String firstName;
     private String lastName;
 
-    // Uncomment when using relationships
-    // private List<Book> books;
-
     // Getters and setters
-    public Long getId() {
-        return id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
 }
